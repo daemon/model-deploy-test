5126a5aac2cccec11667a068f905b20a47ea5d5e
Ralph Tang <r33tang@uwaterloo.ca>
2021-07-06 17:25:47 -0400

    Make checkout script more pretty

--- checkout.sh
+++ checkout.sh
@@ -3,4 +3,5 @@ cd model-deploy-test
 git sparse-checkout init --cone  # fetch all files (not dirs) in the root directory
 git sparse-checkout set mdt  # add the folder mdt to be checked out
 git checkout main  # you're done
+tree .
 du -sch .  # verify that you haven't checked out the models
